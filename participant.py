bc4ede3fd3b333baa3001772b3d88a62a26bbb2d
Siddhant N <71310931+yatocodes@users.noreply.github.com>
2020-10-06 08:31:49 +0530

    Update participant.py
    F String

--- participant.py
+++ participant.py
@@ -1,3 +1,6 @@
 print("Atul Jain: https://www.github.com/atulj968")
 # write your code below
 print('Deepak Kumaar: https://github.com/deepak1011dp')
+
+contributor = 'Siddhant'
+print(f" {contributor} : https://github.com/yatocodes")
