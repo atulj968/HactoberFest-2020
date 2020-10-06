dc8418e26b28519db93f34a55b72ca9c6410c26e
Siddhant N <71310931+yatocodes@users.noreply.github.com>
2020-10-06 08:27:23 +0530

    Update participant.py
    used f string.

--- participant.py
+++ participant.py
@@ -1,3 +1,7 @@
 print("Atul Jain: https://www.github.com/atulj968")
 # write your code below
 print('Deepak Kumaar: https://github.com/deepak1011dp')
+
+contributor = 'Siddhant N'
+#f-String
+print(f"{contributor}: https://github.com/yatocodes")
