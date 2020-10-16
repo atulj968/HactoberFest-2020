dec165b666b6c98f96d9b8967682003aaa148c92
rudranshtripathi9 <72182047+rudranshtripathi9@users.noreply.github.com>
2020-10-16 10:21:05 +0530

    print('Rudransh Tripathi: https://github.com/rudranshtripathi9')

--- participant.py
+++ participant.py
@@ -8,5 +8,5 @@ print('Abhishek Mohata: https://github.com/amohata007')
 print('Ashita Gaur: https://github.com/ashitagaur00')
 print('Ritika Garg: https://github.com/Ritika0126')
 print('Harshit Gupta: https://github.com/harshit257')
-
+print('Rudransh Tripathi: https://github.com/rudranshtripathi9')
 
